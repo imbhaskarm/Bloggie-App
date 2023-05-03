2ea75c60b13b40f3a0b6e9735e249c2a8d456c11
imbhaskarm <bhaskarmaheshwari27@gmail.com>
2023-05-03 09:42:52 +0530

    Initial Commit

---
+++ Bloggie.Web/Data/AuthDbContext.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class AuthDbContext:IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options):base(options)
+        {
+            
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            //seed the roles (User.admin,superadmin)
+
+            var adminRoleId = "8f54bf6e-ac63-45cb-a565-1cd61647e66e";
+            var superAdminRoleId = "415e0755-aff5-4a1e-9eab-65ad2ea8d08f";
+            var userRoleId = "2f1bff2d-7311-4c7b-be28-5605d55ba0b6";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name="Admin",
+                    NormalizedName="Admin",
+                    Id=adminRoleId,
+                    ConcurrencyStamp=adminRoleId
+                },
+                new IdentityRole
+                {
+                    Name="SuperAdmin",
+                    NormalizedName="SuperAdmin",
+                    Id=superAdminRoleId,
+                    ConcurrencyStamp=adminRoleId
+                },
+                new IdentityRole
+                {
+                    Name="User",
+                    NormalizedName="User",
+                    Id=userRoleId,
+                    ConcurrencyStamp=userRoleId
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            //Seed SuperAdminUser
+
+            var superAdminId = "1374ab7b-c7c6-4efc-ac08-7698d15e9ba9";
+            var superAdminUser = new IdentityUser { 
+                UserName="superadmin@bloggie.com",
+                Email="superadmin@bloggie.com",
+                NormalizedEmail="superadmin@bloggie.com".ToUpper(),
+                NormalizedUserName= "superadmin@bloggie.com".ToUpper(),
+                Id=superAdminId
+            };
+
+            superAdminUser.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(superAdminUser, "Superadmin@123");
+            builder.Entity<IdentityUser>().HasData(superAdminUser);
+
+            var superAdminRoles = new List<IdentityUserRole<string>>
+            {
+                new IdentityUserRole<string>
+                {
+                    RoleId=adminRoleId,
+                    UserId=superAdminId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId=superAdminRoleId,
+                    UserId=superAdminId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId=userRoleId,
+                    UserId=superAdminId
+                }
+            };
+
+            builder.Entity<IdentityUserRole<string>>().HasData(superAdminRoles);
+        }
+
+
+    }
+}
---
+++ Bloggie.Web/Migrations/AuthDb/20230501235545_second AuthDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bloggie.Web.Migrations.AuthDb
+{
+    /// <inheritdoc />
+    public partial class secondAuthDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1374ab7b-c7c6-4efc-ac08-7698d15e9ba9",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "0e1d21a1-5dd0-4b4c-93f3-6f668d97e7f5", "AQAAAAIAAYagAAAAEO2H/+B247X4xkF6isKZq6LM734hlaoiwYmrGAIAYqz39seVvZt1Uf85ACtaZ+vhJQ==", "b70b9267-6d3e-48ea-b826-2beed33e56cf" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1374ab7b-c7c6-4efc-ac08-7698d15e9ba9",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "4f6aa8da-e61c-4112-8bff-26bb080421c1", "AQAAAAIAAYagAAAAEL3RJwpf16DS5lxgKP7nDmhQWn18E0Ma+y4XN7SbsiwUhIuM5oIGwhrC2KLpjtlPqA==", "94d863c9-fad4-4145-a095-06c9560091a6" });
+        }
+    }
+}
